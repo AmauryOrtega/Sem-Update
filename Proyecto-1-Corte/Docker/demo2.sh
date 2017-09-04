41b158b1ce0f376f6a32ba15c422f538d1d8a6fa
Amaury Ortega <amauryocortega@gmail.com>
2017-09-04 15:14:24 -0500

    Demo 2 listo

--- Proyecto-1-Corte/Docker/demo2.sh
+++ Proyecto-1-Corte/Docker/demo2.sh
@@ -1,28 +1,24 @@
 #!/bin/bash
-echo "Montando, deteniendo y destruyendo 50 contenedores"
+N=$1
+echo "Montando, deteniendo y destruyendo $N contenedores"
 START=$(date +%s.%N)
-for i in {1..50}
+for id in $(eval echo "{1..$N}")
 do
-	#docker run -d --rm -p 80:80 -p 3306:3306 --name=server$i xxdrackleroxx/test > /dev/null
+	PUERTO_PHP=$((id+2000))
+	PUERTO_SQL=$((id+3000))
+	docker run -d --rm -p $PUERTO_PHP:80 -p $PUERTO_SQL:3306 --name=server$id xxdrackleroxx/test > /dev/null
 done
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
-echo "Tiempo montando: $DIFF"
+echo "Tiempo montando: $DIFF segundos"
 
-echo "\nEsprando 5...4...3...2..1.."
+echo "Esperando 5...4...3...2...1..."
 
 START=$(date +%s.%N)
-for i in {1..50}
+for id in $(eval echo "{1..$N}")
 do
-	#docker stop -t 0 server$i > /dev/null
+	docker stop -t 0 server$id > /dev/null
 done
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
-echo "Tiempo destruyendo: $DIFF"
-
-for i in {1..50}
-do
-	PUERTO_PHP=$((i+2000))
-	PUERTO_SQL=$((i+3000))
-	echo "$i $PUERTO $PUERTO2"
-done
+echo "Tiempo destruyendo: $DIFF segundos"
