f72c4e11d8d9638a6528e17afc998c1175418302
Amaury Ortega <amauryocortega@gmail.com>
2017-09-27 15:32:01 -0500

    comienzo de gson

---
+++ 2-Corte/Proyecto_gson/src/java/controladores/ServidorIniciar.java
@@ -0,0 +1,88 @@
+package controladores;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.DB;
+import modelo.Pc;
+
+public class ServidorIniciar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+
+            DB base_datos = new DB();
+            base_datos.conectar();
+            Pc usuario = base_datos.insertar();
+            
+            //Guardado de variable en sesion
+            request.getSession().setAttribute("pc", usuario);
+            base_datos.desconectar();
+            
+            Process proceso;
+            Runtime shell = Runtime.getRuntime();
+            // COMANDO DOCKER
+            // docker run -d --rm -p [PuertoPHP]:80 -p [PuertoSQL]:3306 --name=server[ID] xxdrackleroxx/test
+            proceso = shell.exec("docker run -d --rm -p " + usuario.getPuertoPHP() + ":80 -p " + usuario.getPuertoSQL() + ":3306 --name=server" + usuario.getId() + " xxdrackleroxx/test");
+            proceso.waitFor();
+            
+            request.getRequestDispatcher("iniciado.jsp").forward(request, response);
+        } catch (Exception e) {
+            request.getSession().setAttribute("mensaje", "Error inesperado, porfavor intente mas tarde");
+            request.getRequestDispatcher("mensaje.jsp").forward(request, response);
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ 2-Corte/Proyecto_gson/src/java/modelo/Util.java
@@ -0,0 +1,5 @@
+package modelo;
+
+public class Util {
+    public static String ip = "192.168.1.105";
+}
