68537fb7bc64afdf457596202b349f489468d119
Amaury Ortega <amauryocortega@gmail.com>
2017-09-27 15:40:49 -0500

    Que hacer

--- 2-Corte/Proyecto_gson/src/java/modelo/Util.java
+++ 2-Corte/Proyecto_gson/src/java/modelo/Util.java
@@ -4,3 +4,8 @@ public class Util {
     public static String ip = "192.168.0.100";
     public static boolean docker = false;
 }
+
+/*
+Re-crea los servlet para que se agregen a la configuracion.
+o mira lo que esta en el JSP para re-crearlo en este
+*/
