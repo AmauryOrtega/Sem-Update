183e47eb0a7a8bb99fc21da72e0ad095c3211fb0
Amaury Ortega <amauryocortega@gmail.com>
2017-10-19 09:55:13 -0500

    Web service funcional

---
+++ 3-Corte/Proyecto_web_service/src/java/ws/WSServidor.java
@@ -0,0 +1,62 @@
+package ws;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jws.Oneway;
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import modelo.DB;
+import modelo.Pc;
+import modelo.Util;
+
+@WebService(serviceName = "WSServidor")
+public class WSServidor {
+
+    @WebMethod(operationName = "iniciarServidor")
+    public String iniciarServidor() {
+        try {
+            DB base_datos = new DB();
+            base_datos.conectar();
+            Pc usuario = base_datos.insertar();
+            base_datos.desconectar();
+
+            if (Util.docker) {
+                Process proceso;
+                Runtime shell = Runtime.getRuntime();
+                // COMANDO DOCKER
+                // docker run -d --rm -p [PuertoPHP]:80 -p [PuertoSQL]:3306 --name=server[ID] xxdrackleroxx/test
+                proceso = shell.exec("docker run -d --rm -p " + usuario.getPuertoPHP() + ":80 -p " + usuario.getPuertoSQL() + ":3306 --name=server" + usuario.getId() + " xxdrackleroxx/test");
+                proceso.waitFor();
+            }
+
+            String json = new Gson().toJson(usuario);
+            return json;
+        } catch (Exception e) {
+            String json = new Gson().toJson("ERROR");
+            return json;
+        }
+    }
+
+    @WebMethod(operationName = "detenerServidor")
+    @Oneway
+    public void detenerServidor(@WebParam(name = "id") String id) {
+        DB base_datos = new DB();
+        base_datos.conectar();
+        base_datos.eliminar(Integer.parseInt(id));
+
+        if (Util.docker) {
+            Process proceso;
+            Runtime shell = Runtime.getRuntime();
+            try {
+                // COMANDO DOCKER
+                // docker run -d --rm -p [PuertoPHP]:80 -p [PuertoSQL]:3306 --name=server[ID] xxdrackleroxx/test:1.0
+                proceso = shell.exec("docker stop -t 0 server" + id);
+            } catch (IOException ex) {
+                System.out.println("[ERROR] Problema con el id obtenido");
+            }
+        }
+    }
+}
