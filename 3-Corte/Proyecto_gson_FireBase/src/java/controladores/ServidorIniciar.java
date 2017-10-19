592e9ea18df319c76d1f057892010382dc85b178
Ramiro Jose Verbel De La Rosa <rajove668@hotmail.com>
2017-10-19 09:41:00 -0500

    clonado de proyectos y modificacion de server. Push aun no funcionando. en proceso...

---
+++ 3-Corte/Proyecto_gson_FireBase/src/java/controladores/ServidorIniciar.java
@@ -0,0 +1,121 @@
+package controladores;
+
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.Writer;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import modelo.DB;
+import modelo.Pc;
+import modelo.Util;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+public class ServidorIniciar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            DB base_datos = new DB();
+            base_datos.conectar();
+            Pc usuario = base_datos.insertar();
+            base_datos.desconectar();
+
+            if (Util.docker) {
+                Process proceso;
+                Runtime shell = Runtime.getRuntime();
+                // COMANDO DOCKER
+                // docker run -d --rm -p [PuertoPHP]:80 -p [PuertoSQL]:3306 --name=server[ID] xxdrackleroxx/test
+                proceso = shell.exec("docker run -d --rm -p " + usuario.getPuertoPHP() + ":80 -p " + usuario.getPuertoSQL() + ":3306 --name=server" + usuario.getId() + " xxdrackleroxx/test");
+                proceso.waitFor();
+            }
+
+            String json = new Gson().toJson(usuario);
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            Writer salida = null;
+            salida = response.getWriter();
+            salida.write(json);
+            salida.close();
+
+            CloseableHttpClient httpclient = HttpClients.createDefault();
+            HttpPost httppost = new HttpPost("https://fcm.googleapis.com/fcm/send");
+            StringEntity jsonPush = new StringEntity("{\"to\": \"c8Yt-yQouZI:APA91bEkZOQLt1jESKmLwkDHD1gu7s-VJ8ThRy8JsHFhpgk3RbfwooiDnyHyhYpdWkylqLyeEHD_tAkoQvm5EyYVE3AumkF_cQwD027M_rTSTQsIpGySbDbY2N9S5OgKUAa2ADRS-GPH\",\n" +
+            "  \"notification\": {\n" +
+            "     \"title\": \"Nuevo Contenedor\",\n" +
+            "     \"body\": \" ID "+ usuario.getId()+" Puertos {SQL: "+usuario.getPuertoSQL()+", PHP: "+usuario.getPuertoPHP()+" \"\n" +
+            "  }}");
+            httppost.addHeader("Authotization", "key=<your legacy server key>");
+            httppost.addHeader("Content-Type", "application/json");
+            httppost.setEntity(jsonPush);
+            CloseableHttpResponse respuesta = httpclient.execute(httppost);
+            System.out.println(respuesta.getStatusLine());
+            respuesta.close();
+
+        } catch (Exception e) {
+            String json = new Gson().toJson("ERROR");
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            Writer salida = null;
+            salida = response.getWriter();
+            salida.write(json);
+            salida.close();
+        }
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
