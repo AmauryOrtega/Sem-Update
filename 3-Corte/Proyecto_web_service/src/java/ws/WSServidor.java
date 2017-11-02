aed539ffb71ab53359d869d83b46f367df33437c
Rajove668 <rajove668@hotmail.com>
2017-11-02 16:45:06 -0500

    Eliminacion de import no usados

--- 3-Corte/Proyecto_web_service/src/java/ws/WSServidor.java
+++ 3-Corte/Proyecto_web_service/src/java/ws/WSServidor.java
@@ -2,8 +2,6 @@ package ws;
 
 import com.google.gson.Gson;
 import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.jws.Oneway;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
